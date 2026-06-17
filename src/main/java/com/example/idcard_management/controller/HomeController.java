96f2e3430f575f2e0a125915aedf8bdc4cd7e3c0
Samnangalex06 <samnangalextt@gmail.com>
2026-06-17 19:36:09 +0700

    Add SQLite test profile and Ansible deploy

--- src/main/java/com/example/idcard_management/controller/HomeController.java
+++ src/main/java/com/example/idcard_management/controller/HomeController.java
@@ -1,4 +1,4 @@
-﻿package com.example.idcard_management.controller;
+package com.example.idcard_management.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
